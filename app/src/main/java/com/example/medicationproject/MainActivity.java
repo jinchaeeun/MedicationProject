87c6ebc2f7b1c5c4329865d46d25000f0f01c205
jinchaeeun <kbg05032@naver.com>
2021-03-13 18:34:52 +0900

    blood Sugar Layout 작성

--- app/src/main/java/com/example/medicationproject/MainActivity.java
+++ app/src/main/java/com/example/medicationproject/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bloodpreslayout);
+        setContentView(R.layout.bloodsugarlayout);
     }
 }
